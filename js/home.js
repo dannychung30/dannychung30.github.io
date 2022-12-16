0df52d1b2d39289462da2b51d9820fa7023e3774
Danny Chung <dannychung6158@gmail.com>
2022-12-16 15:49:30 -0800

    added functionality to burgermenu button

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-let nav = document.getElementById('nav');
+// when burger is clicked, navigation menu slides in from the right
+const right_nav = function () {
+    const burger = document.getElementById('hamburger');
+    const nav = document.getElementById('nav');
+    
+    burger.addEventListener("click", () => {
+        nav.classList.toggle('burgerclicked');
+    });
+}
+
+// calling right_nav function
+const home_page = function () {
+    right_nav();
+}
+
+home_page();
