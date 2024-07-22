acb9dd4a4712e912f1afceea79dc4249dd153f80
dannychung30 <90223301+dannychung30@users.noreply.github.com>
2024-07-21 22:35:50 -0700

    inital commit of new website update

---
+++ dannychung30.github.io-main/js/index.js
@@ -0,0 +1,63 @@
+/*
+    Light-dark-mode toggle switch
+*/
+const toggler = $(".light-dark-toggle > .toggler");
+toggler.on("click", function() {
+    $(this).toggleClass("toggled");
+});
+
+/*
+    Animating the window scroll when clicking on the navigation bar links
+*/
+const anchors = $(".nav-links > .scrolling-links");
+const scroll_to_elements = $(".scrolling-to");
+anchors.each(function(index) {
+    $(this).click(function() {
+        $("body").removeClass("lockscroll");
+        $("html, body").animate({
+            scrollTop: scroll_to_elements.eq(index).offset().top - 100
+        }, 500, "swing");
+    });
+});
+
+
+/*
+    Adding 3D effect to the nav bar once the page has been scrolled down a bit
+*/
+const nav_bar = $(".main-nav");
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 40) {
+        nav_bar.addClass("scrolled-into-page");
+    } else {
+        nav_bar.removeClass("scrolled-into-page");
+    }
+});
+// Triggering the scroll event when the page loads
+$(window).trigger("scroll"); 
+
+
+/*
+    Animating the window scroll when clicking on the Continue Button
+*/
+const continue_btn = $("main .home .continue-btn");
+continue_btn.click(function() {
+    $("body").removeClass("lockscroll");
+    $("html, body").animate({
+        scrollTop: scroll_to_elements.eq(1).offset().top - 100
+    }, 500, "swing");
+});
+
+
+/*
+    Keep label at top of the input if there is text inside the input element
+*/
+const form_inputs = $(".contact .contact-me-form > .input-wrapper > :is(input, textarea)");
+form_inputs.each(function() {
+    $(this).keyup(function() {
+        if ($(this).is(":focus") && $(this).val() != "") {
+            $(this).next("label").addClass("focused-with-text");
+        } else {
+            $(this).next("label").removeClass("focused-with-text");
+        }
+    })
+});
